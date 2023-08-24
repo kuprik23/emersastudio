25eec99d1226414ed2c4bd54ea75d0a6409a2a60
kuprik23 <martkrupik@gmail.com>
2023-08-25 00:07:56 +0100

    updt

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cube = document.getElementById("cube");
 const colorInput = document.getElementById("color");
+const hexColorInput = document.getElementById("hex-color");
 const sizeSlider = document.getElementById("size");
 const personalitySlider = document.getElementById("personality");
 const generateCodeButton = document.getElementById("generate-code");
@@ -16,27 +17,14 @@ function updateCube() {
     cube.style.width = `${sizeValue * 50}px`;
     cube.style.height = `${sizeValue * 50}px`;
     cube.style.borderRadius = `${personalityValue * 10}%`;
+
+    // Update the hex color input
+    hexColorInput.value = colorValue;
 }
 
 // Event listeners for input changes
 colorInput.addEventListener("input", updateCube);
-sizeSlider.addEventListener("input", updateCube);
-personalitySlider.addEventListener("input", updateCube);
-
-// Function to generate code based on the slider values
-function generateCode() {
-    const colorValue = colorInput.value;
-    const sizeValue = sizeSlider.value;
-    const personalityValue = personalitySlider.value;
-
-    // Generate code based on the slider values
-    const code = `
-        // Create a customizable cube with the selected properties
-        const cube = document.getElementById("cube");
-        cube.style.backgroundColor = "${colorValue}";
-        cube.style.width = "${sizeValue * 50}px";
-        cube.style.height = "${sizeValue * 50}px";
-        cube.style.borderRadius = "${personalityValue * 10}%";
-    `;
-
-    //
+hexColorInput.addEventListener("input", () => {
+    // Ensure the hex color input is in the correct format (#RRGGBB)
+    const hexColor = hexColorInput.value;
+    if (/^#([0-9A
