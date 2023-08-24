0f570403364475e9f7efd7dc7d440c0a95a3abb8
kuprik23 <martkrupik@gmail.com>
2023-08-25 00:12:05 +0100

    updt3

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const cube = document.getElementById("cube");
 const colorInput = document.getElementById("color");
-const hexColorInput = document.getElementById("hex-color");
 const sizeSlider = document.getElementById("size");
 const personalitySlider = document.getElementById("personality");
 const generateCodeButton = document.getElementById("generate-code");
@@ -17,14 +16,41 @@ function updateCube() {
     cube.style.width = `${sizeValue * 50}px`;
     cube.style.height = `${sizeValue * 50}px`;
     cube.style.borderRadius = `${personalityValue * 10}%`;
-
-    // Update the hex color input
-    hexColorInput.value = colorValue;
 }
 
 // Event listeners for input changes
 colorInput.addEventListener("input", updateCube);
-hexColorInput.addEventListener("input", () => {
-    // Ensure the hex color input is in the correct format (#RRGGBB)
-    const hexColor = hexColorInput.value;
-    if (/^#([0-9A
+sizeSlider.addEventListener("input", updateCube);
+personalitySlider.addEventListener("input", updateCube);
+
+// Function to generate pseudo code based on the slider values
+function generateCode() {
+    const colorValue = colorInput.value;
+    const sizeValue = sizeSlider.value;
+    const personalityValue = personalitySlider.value;
+
+    // Generate pseudo code based on the slider values
+    const code = `
+        // Create a customizable 3D cube with the following properties:
+        // Color: "${colorValue}"
+        // Size: ${sizeValue}
+        // Personality: ${personalityValue}
+        
+        // Add your code to create the cube here
+    `;
+
+    // Display the generated pseudo code
+    generatedCode.value = code;
+}
+
+// Event listener for the Generate Pseudo Code button
+generateCodeButton.addEventListener("click", generateCode);
+
+// Function to copy generated pseudo code to the clipboard
+function copyCode() {
+    generatedCode.select();
+    document.execCommand("copy");
+}
+
+// Event listener for the Copy Code button
+copyCodeButton.addEventListener("click", copyCode);
