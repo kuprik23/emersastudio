f8aa756e8dc520c679f0df5347c60e362e3ff6cc
kuprik23 <martkrupik@gmail.com>
2023-08-24 23:59:25 +0100

    updt

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-// Initialize Three.js variables
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const cube = document.getElementById("cube");
+const rotationSlider = document.getElementById("rotation");
+const sizeSlider = document.getElementById("size");
+const personalitySlider = document.getElementById("personality");
+const generateCodeButton = document.getElementById("generate-code");
+const copyCodeButton = document.getElementById("copy-code");
+const generatedCode = document.getElementById("generated-code");
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x3498db });
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-
-camera.position.z = 5;
-
-// Function to update the cube's rotation and shape based on the slider values
+// Function to update the cube's rotation, size, and shape based on the slider values
 function updateCube() {
-    const rotationValue = typeSlider.value;
-    cube.rotation.x = (rotationValue * Math.PI) / 180; // Convert degrees to radians
-
+    const rotationValue = rotationSlider.value;
+    const sizeValue = sizeSlider.value;
     const personalityValue = personalitySlider.value;
-    // Change cube's shape based on personality slider (e.g., scale it)
-    cube.scale.set(1, 1, personalityValue);
+
+    cube.style.transform = `rotateX(${rotationValue}deg) rotateY(${rotationValue}deg) scale(${sizeValue})`;
+    cube.style.borderRadius = `${personalityValue * 10}%`;
 }
 
 // Event listeners for slider changes
-const typeSlider = document.getElementById("type");
-typeSlider.addEventListener("input", updateCube);
-
-const personalitySlider = document.getElementById("personality");
+rotationSlider.addEventListener("input", updateCube);
+sizeSlider.addEventListener("input", updateCube);
 personalitySlider.addEventListener("input", updateCube);
 
 // Function to generate code based on the slider values
 function generateCode() {
-    const rotationValue = typeSlider.value;
+    const rotationValue = rotationSlider.value;
+    const sizeValue = sizeSlider.value;
     const personalityValue = personalitySlider.value;
 
     // Generate code based on the slider values
     const code = `
         // Add code here to create a rotating cube with the selected properties
         // Rotation: ${rotationValue} degrees
+        // Size: ${sizeValue}
         // Personality: ${personalityValue}
     `;
 
     // Display the generated code
-    generatedCode.textContent = code;
+    generatedCode.value = code;
 }
 
 // Event listener for the Generate Code button
-const generateCodeButton = document.getElementById("generate-code");
 generateCodeButton.addEventListener("click", generateCode);
 
-// Function to animate the cube
-function animate() {
-    requestAnimationFrame(animate);
-
-    renderer.render(scene, camera);
+// Function to copy generated code to the clipboard
+function copyCode() {
+    generatedCode.select();
+    document.execCommand("copy");
 }
 
-animate();
+// Event listener for the Copy Code button
+copyCodeButton.addEventListener("click", copyCode);
